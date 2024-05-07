58695bc64a5ae874bc40a88c440090e912772e17
sohosoki <so.hosoki@enechange.co.jp>
2024-05-07 15:40:33 +0900

    get answer

--- gas/src/app.ts
+++ gas/src/app.ts
@@ -1,5 +1,22 @@
 
 declare let global: any;
 global.onFromSubmit = (e: GoogleAppsScript.Events.FormsOnFormSubmit) => {
-  console.log(e);
+  const answers =  e.response.getItemResponses().map(res => {
+    return {
+      title: res.getItem().getTitle(), 
+      type: res.getItem().getType().toString(),
+      values: res.getResponse() 
+    };
+  }).filter(v => v);
+
+  console.log(answers);
 };
+
+const extractFileIdFromUrl = (url: string) => {
+  const match = /drive\.google\.com\/file\/d\/([a-zA-Z0-9_-]+)/.exec(url);
+  if (match) {
+    return match[1];
+  } else {
+    return null;
+  }
+}
